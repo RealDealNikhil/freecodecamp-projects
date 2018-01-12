a22e513346ec1acf672424231484f60cb8577036
Nikhil Suri <nikhilsuri@comcast.net>
2018-01-11 18:49:37 -0800

    first node server

---
+++ node-projects/simpleServer.js
@@ -0,0 +1,12 @@
+// net and strftime modules
+var net = require('net');
+var strftime = require('strftime');
+
+// create server and write current date to socket
+var server = net.createServer(function(socket) {
+    var date = strftime('%F %H:%M', new Date());
+    socket.end(date + "\n");
+});
+
+// listen for connections
+server.listen(process.argv[2]);
